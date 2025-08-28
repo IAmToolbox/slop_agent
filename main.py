ac857e80ec6520494958aef3be4fc34610bcd652
IAmToolbox <danielalejandro.leivafunes9@gmail.com>
2025-08-27 22:22:51 -0500

    Slopped up the main file with a sloppy list to save all the slop. Also the slop can be verbose too I guess...

--- main.py
+++ main.py
@@ -1,9 +1,33 @@
 import os
+import sys
 from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
+
+load_dotenv()
+api_key = os.environ.get("GEMINI_API_KEY")
+client = genai.Client(api_key=api_key)
+if len(sys.argv) != 1:
+    user_prompt = sys.argv[1]
+verbose = "--verbose" in sys.argv
+
+messages = [types.Content(role="user", parts=[types.Part(text=user_prompt)])]
 
 
 def main():
-    print("Hello from slop-agent!")
+    if len(sys.argv) == 1:
+        raise Exception("prompt not provided")
+    content_response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages,
+    )
+
+    print(content_response.text)
+    if verbose:
+        print(f"User prompt: {user_prompt}")
+        print(f"Prompt tokens: {content_response.usage_metadata.prompt_token_count}")
+        print(f"Response tokens: {content_response.usage_metadata.candidates_token_count}")
 
 
 if __name__ == "__main__":
