b0f807c56332b95920a741fd27d99773b2294e96
IAmToolbox <danielalejandro.leivafunes9@gmail.com>
2025-09-03 21:45:45 -0500

    I made the slop stupid for the bit

--- functions/run_python_file.py
+++ functions/run_python_file.py
@@ -4,7 +4,7 @@ import subprocess
 def run_python_file(working_directory, file_path, args=[]):
     abs_path = os.path.abspath(os.path.join(working_directory, file_path))
     if working_directory not in abs_path:
-        return f'Error: Cannot execute to "{file_path}" as it is outside the permitted working directory'
+        return f'Error: Cannot execute "{file_path}" as it is outside the permitted working directory'
     if not os.path.exists(abs_path):
         return f'Error: File "{file_path}" not found.'
     if not file_path.endswith(".py"):
